6f12f8919cecc499d3d2f68a372395dc9221b737
khaledmo07 <kmoamed942@gmail.com>
2026-03-23 00:23:56 +0200

    add Contetx

---
+++ FitTrack Pro/Models/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿
+namespace FitTrack_Pro.Models
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public DbSet<Member> Members { get; set; }
+
+        public DbSet<GymClass> GymClasses { get; set; }
+        public DbSet<ClassAttendance> classAttendances { get; set; }
+        public ApplicationDbContext()
+        {
+            
+        }
+    }
+}
