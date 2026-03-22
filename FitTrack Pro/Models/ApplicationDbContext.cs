205caf76753a77f733b1065cc2628c7b25ea9b50
khaledmo07 <kmoamed942@gmail.com>
2026-03-23 00:26:58 +0200

    add context

--- FitTrack Pro/Models/ApplicationDbContext.cs
+++ FitTrack Pro/Models/ApplicationDbContext.cs
@@ -4,8 +4,13 @@ namespace FitTrack_Pro.Models
     public class ApplicationDbContext:DbContext
     {
         public DbSet<Member> Members { get; set; }
-
+        public DbSet<Trainer> Trainers { get; set; }
         public DbSet<GymClass> GymClasses { get; set; }
+        public DbSet<SubscriptionPlan> SubscriptionPlans { get; set; }
+
+        public DbSet<MemberSubscription> MembersSubscription { get; set; }
+
+
         public DbSet<ClassAttendance> classAttendances { get; set; }
         public ApplicationDbContext()
         {
