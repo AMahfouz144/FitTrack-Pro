acfb8a58fba5df6b990f50dec87bd8f83c03fafb
Abdo Mahfouz <abdomohmmedmahfouz@gmail.com>
2026-03-30 22:01:58 +0200

    fix bug in memeber form

--- FitTrack Pro/ViewModels/Memberviewmodels.cs
+++ FitTrack Pro/ViewModels/Memberviewmodels.cs
@@ -99,15 +99,17 @@ namespace FitTrack_Pro.ViewModels
 		[EmailAddress(ErrorMessage = "Invalid email address")]
 		[Display(Name = "Email/Username")]
 		public string? UserName { get; set; }
-
+		[Required(ErrorMessage = "Password is required")]
 		[DataType(DataType.Password)]
 		[StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
 		public string? Password { get; set; }
 
 		[DataType(DataType.Password)]
 		[Display(Name = "Confirm password")]
+		[Required(ErrorMessage = "Confirm Password is required")]
 		[Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
 		public string? ConfirmPassword { get; set; }
+		
 	}
 
 	// ════════════════════════════════════════════════════════════════
